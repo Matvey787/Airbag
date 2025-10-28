29db6167381869f5783d1af2864ab8a610bbef8f
Matvey787 <matveyklg@gmail.com>
2025-10-28 18:24:26 +0300

    egor

--- airbag.ino
+++ airbag.ino
@@ -2,6 +2,12 @@
 #include "sbus.h"  // Библиотека Bolder Flight Systems SBUS
 
 
+enum class side_t 
+{
+    RIGHT,
+    LEFT,
+    NO_SIDE
+};
 
 // Пины esp32 отвечающие за посылку PPM на esc двигателей
 const int ESC_MAIN_ENGINE_PIN  = 13;
@@ -28,11 +34,19 @@ const int ESC_MAIN_ENGINE_FULL_THROTTLE = 2000;
 
 // Каналы управления (нумерация с 0)
 const int MAIN_ENGINE_CHANNEL = 10;
-const int LEFT_ENGINE_CHANNEL = 2;
-const int RIGHT_ENGINE_CHANNEL = 3;
+const int MAIN_ENGINE_MIN_CHANNEL_VALUE = 172;
+const int MAIN_ENGINE_MAX_CHANNEL_VALUE = 1100;
 
-const int MIN_CHANNEL_VALUE = 172;
-const int MAX_CHANNEL_VALUE = 1700;
+const int FORWARD_THROTTLE_CHANNEL = 2;
+const int FORWARD_MIN_CHANNEL_VALUE = 172;
+const int FORWARD_MAX_CHANNEL_VALUE = 1800;
+
+const int ROTATE_THROTTLE_CHANNEL = 0;
+const int ROTATE_ENGINE_MIN_CHANNEL_VALUE = 172;
+const int ROTATE_ENGINE_MIDDLE_CHANNEL_VALUE = 1000;
+const int ROTATE_ENGINE_MAX_CHANNEL_VALUE = 1800;
+
+const int ARM_CHANNEL = 4;
 
 // Создаём объект приёмника SBUS для ESP32
 // Пояснения:
@@ -80,25 +94,76 @@ void loop() {
 
     bool failsafe = data.failsafe;
 
-    // переводим данные с канала в PPM
-    int esc_me_Value = map(channels[MAIN_ENGINE_CHANNEL],  MIN_CHANNEL_VALUE, MAX_CHANNEL_VALUE, ESC_MAIN_ENGINE_OFF, ESC_MAIN_ENGINE_FULL_THROTTLE);
-    int esc_le_Value = map(channels[LEFT_ENGINE_CHANNEL],  MIN_CHANNEL_VALUE, MAX_CHANNEL_VALUE, ESC_LR_ENGINE_OFF,   ESC_LR_ENGINE_FULL_THROTTLE  );
-    int esc_re_Value = map(channels[RIGHT_ENGINE_CHANNEL], MIN_CHANNEL_VALUE, MAX_CHANNEL_VALUE, ESC_LR_ENGINE_OFF,   ESC_LR_ENGINE_FULL_THROTTLE  );
+    // channel value -> PPM
+    int esc_me_Value = map(channels[MAIN_ENGINE_CHANNEL],  MAIN_ENGINE_MIN_CHANNEL_VALUE, MAIN_ENGINE_MAX_CHANNEL_VALUE, ESC_MAIN_ENGINE_OFF, ESC_MAIN_ENGINE_FULL_THROTTLE);
+    int esc_le_Value = map(channels[FORWARD_THROTTLE_CHANNEL],  FORWARD_MIN_CHANNEL_VALUE, FORWARD_MAX_CHANNEL_VALUE, ESC_LR_ENGINE_OFF,   ESC_LR_ENGINE_FULL_THROTTLE  );
+    int esc_re_Value = map(channels[FORWARD_THROTTLE_CHANNEL], FORWARD_MIN_CHANNEL_VALUE, FORWARD_MAX_CHANNEL_VALUE, ESC_LR_ENGINE_OFF,   ESC_LR_ENGINE_FULL_THROTTLE  );
 
-    if (failsafe) // failsafe
+    int rotate_channel_cur = channels[ROTATE_THROTTLE_CHANNEL];
+
+    side_t side = side_t::NO_SIDE;
+    if ((rotate_channel_cur - ROTATE_ENGINE_MIDDLE_CHANNEL_VALUE) < -50)
+    {
+      side = side_t::RIGHT;
+    }
+    else if ((rotate_channel_cur - ROTATE_ENGINE_MIDDLE_CHANNEL_VALUE) > 50)
+    {
+      side = side_t::LEFT;
+    }
+    
+
+    // right rotate
+    if (side == side_t::RIGHT)
+    {
+        int ppm_rotate_channel_cur = map(abs(1000 - rotate_channel_cur) + ROTATE_ENGINE_MIN_CHANNEL_VALUE, ROTATE_ENGINE_MIN_CHANNEL_VALUE, ROTATE_ENGINE_MIDDLE_CHANNEL_VALUE, 0, 500);
+        esc_re_Value += ppm_rotate_channel_cur;
+        esc_le_Value -= ppm_rotate_channel_cur;
+
+    }
+    // left rotate
+    else if (side == side_t::LEFT)
+    {
+        int ppm_rotate_channel_cur = map(abs(rotate_channel_cur - 1000) + ROTATE_ENGINE_MIDDLE_CHANNEL_VALUE, ROTATE_ENGINE_MIDDLE_CHANNEL_VALUE, ROTATE_ENGINE_MAX_CHANNEL_VALUE, 0, 500);
+        esc_re_Value -= ppm_rotate_channel_cur;
+        esc_le_Value += ppm_rotate_channel_cur;  
+    }
+
+    // check limits
+    if (esc_re_Value < ESC_LR_ENGINE_OFF)
+        esc_re_Value = ESC_LR_ENGINE_OFF;
+    else if (esc_re_Value > ESC_LR_ENGINE_FULL_THROTTLE)
+        esc_re_Value = ESC_LR_ENGINE_FULL_THROTTLE;
+
+
+    if (esc_le_Value < ESC_LR_ENGINE_OFF)
+        esc_le_Value = ESC_LR_ENGINE_OFF;
+    else if (esc_le_Value > ESC_LR_ENGINE_FULL_THROTTLE)
+        esc_le_Value = ESC_LR_ENGINE_FULL_THROTTLE;
+        
+    // failsafe    
+    if (failsafe)
+    {
+      esc_me_Value = ESC_MAIN_ENGINE_OFF;
+      esc_le_Value = ESC_LR_ENGINE_OFF;
+      esc_re_Value = ESC_LR_ENGINE_OFF;
+    }
+    if (channels[ARM_CHANNEL] < 1000)
     {
       esc_me_Value = ESC_MAIN_ENGINE_OFF;
       esc_le_Value = ESC_LR_ENGINE_OFF;
       esc_re_Value = ESC_LR_ENGINE_OFF;
     }
     
-    Serial.print("me CHANNEL VALUE:"); Serial.print(channels[MAIN_ENGINE_CHANNEL]);
-    Serial.print(" PPM: "); Serial.println(esc_me_Value);
-    Serial.print("    le CHANNEL VALUE:"); Serial.print(channels[LEFT_ENGINE_CHANNEL]);
-    Serial.print(" PPM: "); Serial.println(esc_le_Value);
-    Serial.print("    re CHANNEL VALUE:"); Serial.print(channels[RIGHT_ENGINE_CHANNEL]);
-    Serial.print(" PPM: "); Serial.println(esc_re_Value);
+    // debug info
+    // Serial.print("me CHANNEL VALUE:"); Serial.print(channels[MAIN_ENGINE_CHANNEL]);
+    // Serial.print("    forward ch val"); Serial.print(channels[FORWARD_THROTTLE_CHANNEL]);
+    // Serial.print("    rotate ch val"); Serial.print(channels[ROTATE_THROTTLE_CHANNEL]);
+    // Serial.print("main PPM: "); Serial.println(esc_me_Value);
+    // Serial.print("    right PPM: "); Serial.println(esc_re_Value);
+    // Serial.print("    left PPM: "); Serial.println(esc_le_Value);
+    Serial.print("    arm: "); Serial.println(channels[ARM_CHANNEL]);
     
+    // load new ppm
     esc_me.writeMicroseconds(esc_me_Value);
     esc_le.writeMicroseconds(esc_le_Value);
     esc_re.writeMicroseconds(esc_re_Value);
